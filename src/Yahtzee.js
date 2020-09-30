341da2f8a85c825febf0266b4ee2a1cf5c4f6f43
Andras Karasz <andras.karasz@logmein.com>
2020-09-30 21:37:49 +0200

    fix: broken websocket link

--- src/Yahtzee.js
+++ src/Yahtzee.js
@@ -79,7 +79,7 @@ class Yahtzee extends React.Component {
         }
       })
       .then((__) => {
-        const ws = new WebSocket(config.baseUri.ws + this.props.game + '/ws')
+        const ws = new WebSocket(config.baseUri.ws + '/' + this.props.game + '/ws')
 
         ws.onmessage = (e) => {
           this.setState(JSON.parse(e.data))
