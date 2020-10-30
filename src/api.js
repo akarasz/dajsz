80b3d7762be2e95ee2cd1cc56954c2814905cacf
Andras Karasz <andras.karasz@logmein.com>
2020-10-30 13:47:28 +0100

    fix: utf8 character encoding in player names

--- src/api.js
+++ src/api.js
@@ -1,8 +1,18 @@
 import config from './config.js'
 
+function b64EncodeUnicode(str) {
+    // first we use encodeURIComponent to get percent-encoded UTF-8,
+    // then we convert the percent encodings into raw bytes which
+    // can be fed into btoa.
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+        function toSolidBytes(match, p1) {
+            return String.fromCharCode('0x' + p1);
+    }));
+}
+
 const headers = (user) => (
   new Headers({
-    "Authorization": "Basic " + btoa(user + ":"),
+    "Authorization": "Basic " + b64EncodeUnicode(user + ":"),
   })
 )
 
