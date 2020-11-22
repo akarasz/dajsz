8f67e618ff0b012812039dd0494e211b6150692c
Andras Karasz <andras.karasz@logmein.com>
2020-11-22 15:48:12 +0100

    fix: use new events schema

--- src/Yahtzee.jsx
+++ src/Yahtzee.jsx
@@ -85,7 +85,7 @@ class Yahtzee extends React.Component {
         const ws = new WebSocket(config.baseUri.ws + '/' + this.props.game + '/ws')
 
         ws.onmessage = (e) => {
-          this.setState(JSON.parse(JSON.parse(e.data)))
+          this.setState(JSON.parse(JSON.parse(e.data)).Data)
         }
       })
   }
